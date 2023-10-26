a22c9b7798be31ae94faa9fc5b9140a3b628308b
Deepak V S <deepaksanjeev1998@gmail.com>
2023-10-26 19:50:58 +0530

    a change is made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SingleProduct from './components/SingleProduct/SingleProduct';
+import SingleProduct from './components/SingleProduct/SingleProduct' ;
 const Home = lazy(() => import('./components/Home/Home.jsx'));
 const Cart=lazy(()=> import('./components/Cart/Cart.jsx'));
 const Order=lazy(()=>import('./components/Order/Order.jsx'));
